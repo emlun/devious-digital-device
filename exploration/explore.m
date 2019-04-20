daaa9fd6e4f12e2bce741485cb4bf01bfeddade1
Emil Lundberg <emil@emlun.se>
2019-04-21 01:34:35 +0200

    Update QR code to real flag

--- exploration/explore.m
+++ exploration/explore.m
@@ -3,27 +3,27 @@ global solution;
 M = 1;
 _ = 0;
 qr_code = [
-    M M M M M M M _ _ M M _ _ _ M M M M M M M
-    M _ _ _ _ _ M _ _ _ M M M _ M _ _ _ _ _ M
-    M _ M M M _ M _ _ M _ M _ _ M _ M M M _ M
-    M _ M M M _ M _ M M _ _ _ _ M _ M M M _ M
-    M _ M M M _ M _ M M _ M _ _ M _ M M M _ M
-    M _ _ _ _ _ M _ _ M _ M M _ M _ _ _ _ _ M
+    M M M M M M M _ M _ _ _ _ _ M M M M M M M
+    M _ _ _ _ _ M _ M M _ _ M _ M _ _ _ _ _ M
+    M _ M M M _ M _ M _ M _ _ _ M _ M M M _ M
+    M _ M M M _ M _ M _ _ M M _ M _ M M M _ M
+    M _ M M M _ M _ _ M M M _ _ M _ M M M _ M
+    M _ _ _ _ _ M _ M M _ M M _ M _ _ _ _ _ M
     M M M M M M M _ M _ M _ M _ M M M M M M M
-    _ _ _ _ _ _ _ _ _ M _ _ M _ _ _ _ _ _ _ _
-    M M _ _ _ M M M _ _ _ M M _ _ _ M M _ _ _
-    _ _ _ M _ M _ _ _ _ M _ _ M _ M M _ M _ _
-    M _ M M M _ M M _ M M _ M M M _ _ M M M _
-    M M M _ _ M _ M _ _ M _ M M _ _ _ M M M M
-    _ M _ M M M M _ M _ _ M M M _ M M M _ _ M
-    _ _ _ _ _ _ _ _ M _ M M M M M M M _ _ M M
-    M M M M M M M _ M _ M _ M _ M _ _ M _ M _
-    M _ _ _ _ _ M _ M _ _ M _ M M M M _ M _ M
-    M _ M M M _ M _ _ _ M M _ _ M _ _ M _ M M
-    M _ M M M _ M _ _ _ M M M M _ M M _ M _ _
-    M _ M M M _ M _ _ _ _ _ M _ M _ M _ _ M M
-    M _ _ _ _ _ M _ M M _ _ M _ _ M M M M _ _
-    M M M M M M M _ M _ M _ M M M _ M M M M _
+    _ _ _ _ _ _ _ _ _ M M M _ _ _ _ _ _ _ _ _
+    M M _ _ M M M _ _ _ _ M M _ _ M _ M M M M
+    M _ _ _ _ M _ _ _ M M M _ _ M M M _ _ M _
+    M M M M M _ M M _ _ M _ _ _ _ _ M _ _ M _
+    M _ _ M _ M _ M M M _ M M M _ M M _ _ M _
+    M _ M M _ M M _ _ M _ M M _ _ _ M _ _ _ _
+    _ _ _ _ _ _ _ _ M _ M _ _ M _ _ M M M M _
+    M M M M M M M _ _ M M _ _ M M _ M M _ M _
+    M _ _ _ _ _ M _ M _ M _ _ M _ _ _ _ _ M _
+    M _ M M M _ M _ M M _ _ M _ _ M _ _ _ M M
+    M _ M M M _ M _ _ M M M _ M M _ M M _ _ M
+    M _ M M M _ M _ _ M M M _ M M _ M M M _ _
+    M _ _ _ _ _ M _ M M _ M _ M _ _ M _ _ _ _
+    M M M M M M M _ M _ _ _ _ M _ _ M _ _ _ M
 ];
 
 % randperm(16) >= 9
@@ -105,27 +105,27 @@ function [m] = led_matrix (x)
   z8 = z(8);
 
   m = [
-    a1 a1  a1  a1  a1  a1  a1 z1 z2 o1 o2 z3 z4 z5 a2 a2  a2  a2  a2  a2 a2
-    a1 b1  b2  b3  b4  b5  a1 z6 z7 z8 o3 o4 o5 z1 a2 b13 b14 b15 b16 b1 a2
-    a1 b16 a1  a1  a1  b6  a1 z2 z3 o6 z4 o7 z5 z6 a2 b12 a2  a2  a2  b2 a2
-    a1 b15 a1  a1  a1  b7  a1 z7 o8 o1 z8 z1 z2 z3 a2 b11 a2  a2  a2  b3 a2
-    a1 b14 a1  a1  a1  b8  a1 z4 o2 o3 z5 o4 z6 z7 a2 b10 a2  a2  a2  b4 a2
-    a1 b13 b12 b11 b10 b9  a1 z8 z1 o5 z2 o6 o7 z3 a2 b9  b8  b7  b6  b5 a2
-    a1 a1  a1  a1  a1  a1  a1 z4 o8 z5 o1 z6 o2 z7 a2 a2  a2  a2  a2  a2 a2
-    z8 z1  z2  z3  z4  z5  z6 z7 z8 o3 z1 z2 o4 z3 z4 z5  z6  z7  z8  z1 z2
-    o5 o6  z3  z4  z5  o7  o8 o1 z6 z7 z8 o2 o3 z1 z2 z3  o4  o5  z4  z5 z6
-    z7 z8  z1  o6  z2  o7  z3 z4 z5 z6 o8 z7 z8 o1 z1 o2  o3  z2  o4  z3 z4
-    o5 z5  o6  o7  o8  z6  o1 o2 z7 o3 o4 z8 o5 o6 o7 z1  z2  o8  o1  o2 z3
-    o3 o4  o5  z4  z5  o6  z6 o7 z7 z8 o8 z1 o1 o2 z2 z3  z4  o3  o4  o5 o6
-    z5 o7  z6  o8  o1  o2  o3 z7 o4 z8 z1 o5 o6 o7 z2 o8  o1  o2  z3  z4 o3
-    z5 z6  z7  z8  z1  z2  z3 z4 o4 z5 o5 o6 o7 o8 o1 o2  o3  z6  z7  o4 o5
-    a3 a3  a3  a3  a3  a3  a3 z8 o6 z1 o7 z2 o8 z3 o1 z4  z5  o2  z6  o3 z7
-    a3 b5  b6  b7  b8  b9  a3 z8 o4 z1 z2 o5 z3 o6 o7 o8  o1  z4  o2  z5 o3
-    a3 b4  a3  a3  a3  b10 a3 z6 z7 z8 o4 o5 z1 z2 o6 z3  z4  o7  z5  o8 o1
-    a3 b3  a3  a3  a3  b11 a3 z6 z7 z8 o2 o3 o4 o5 z1 o6  o7  z2  o8  z3 z4
-    a3 b2  a3  a3  a3  b12 a3 z5 z6 z7 z8 z1 o1 z2 o2 z3  o3  z4  z5  o4 o5
-    a3 b1  b16 b15 b14 b13 a3 z6 o6 o7 z7 z8 o8 z1 z2 o1  o2  o3  o4  z3 z4
-    a3 a3  a3  a3  a3  a3  a3 z5 o5 z6 o6 z7 o7 o8 o1 z8  o2  o3  o4  o5 z1
+    a1 a1  a1  a1  a1  a1  a1 z1 o1 z2 z3 z4 z5 z6 a2 a2  a2  a2  a2  a2 a2
+    a1 b1  b2  b3  b4  b5  a1 z7 o2 o3 z8 z1 o4 z2 a2 b13 b14 b15 b16 b1 a2
+    a1 b16 a1  a1  a1  b6  a1 z3 o5 z4 o6 z5 z6 z7 a2 b12 a2  a2  a2  b2 a2
+    a1 b15 a1  a1  a1  b7  a1 z8 o7 z1 z2 o8 o1 z3 a2 b11 a2  a2  a2  b3 a2
+    a1 b14 a1  a1  a1  b8  a1 z4 z5 o2 o3 o4 z6 z7 a2 b10 a2  a2  a2  b4 a2
+    a1 b13 b12 b11 b10 b9  a1 z8 o5 o6 z1 o7 o8 z2 a2 b9  b8  b7  b6  b5 a2
+    a1 a1  a1  a1  a1  a1  a1 z3 o1 z4 o2 z5 o3 z6 a2 a2  a2  a2  a2  a2 a2
+    z7 z8  z1  z2  z3  z4  z5 z6 z7 o4 o5 o6 z8 z1 z2 z3  z4  z5  z6  z7 z8
+    o7 o8  z1  z2  o1  o2  o3 z3 z4 z5 z6 o4 o5 z7 z8 o6  z1  o7  o8  o1 o2
+    o3 z2  z3  z4  z5  o4  z6 z7 z8 o5 o6 o7 z1 z2 o8 o1  o2  z3  z4  o3 z5
+    o4 o5  o6  o7  o8  z6  o1 o2 z7 z8 o3 z1 z2 z3 z4 z5  o4  z6  z7  o5 z8
+    o6 z1  z2  o7  z3  o8  z4 o1 o2 o3 z5 o4 o5 o6 z6 o7  o8  z7  z8  o1 z1
+    o2 z2  o3  o4  z3  o5  o6 z4 z5 o7 z6 o8 o1 z7 z8 z1  o2  z2  z3  z4 z5
+    z6 z7  z8  z1  z2  z3  z4 z5 o3 z6 o4 z7 z8 o5 z1 z2  o6  o7  o8  o1 z3
+    a3 a3  a3  a3  a3  a3  a3 z4 z5 o2 o3 z6 z7 o4 o5 z8  o6  o7  z1  o8 z2
+    a3 b5  b6  b7  b8  b9  a3 z3 o1 z4 o2 z5 z6 o3 z7 z8  z1  z2  z3  o4 z4
+    a3 b4  a3  a3  a3  b10 a3 z5 o5 o6 z6 z7 o7 z8 z1 o8  z2  z3  z4  o1 o2
+    a3 b3  a3  a3  a3  b11 a3 z5 z6 o3 o4 o5 z7 o6 o7 z8  o8  o1  z1  z2 o2
+    a3 b2  a3  a3  a3  b12 a3 z3 z4 o3 o4 o5 z5 o6 o7 z6  o8  o1  o2  z7 z8
+    a3 b1  b16 b15 b14 b13 a3 z1 o3 o4 z2 o5 z3 o6 z4 z5  o7  z6  z7  z8 z1
+    a3 a3  a3  a3  a3  a3  a3 z2 o8 z3 z4 z5 z6 o1 z7 z8  o2  z1  z2  z3 o3
   ];
 
 end
